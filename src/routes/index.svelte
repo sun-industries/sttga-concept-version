610827e220e302e26002016d2bee311b8333459a
= <r.gnzlz.cstll@gmail.com>
2022-10-20 17:38:12 -0300

    first commit of the concept version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,12 +8,13 @@
 <div class="w-full h-full m-0 overflow-hidden">
 	<div class="fixed bottom-11 right-0 p-1 pl-4 text-blue-gray-900 bg-white bg-opacity-95 z-50">
 		<div class="flex flex-row">
-			<div class="pr-2 text-center">
+			<div class="pr-2 text-center flex items-center">
 				<a href="https://sun.industries">
-				<img src={logo10} class="w-11 m-auto lg:m-0" alt="Hypergea Logo" />
+				<img src={logo10} class="h-4 m-auto lg:m-0" alt="Hypergea Logo" />
 				</a>
 			</div>
-			<div class="w-32 pl-2 border-l border-solid border-blue-gray-900 text-xs leading-none">
+			<div class="w-32 pl-2 border-l border-solid border-blue-gray-900 text-xs leading-none
+			flex items-center">
 				<small>Satellite Tracking Tool for Ground Astronomy</small>
 			</div>
 		</div>
